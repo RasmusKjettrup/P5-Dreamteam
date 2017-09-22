d46f3761a5ae46e1396e758e6d506ba69f393ec7
Jeppe Lindberg <jepp3441@gmail.com>
2017-09-22 11:30:56 +0200

    Breadth first search, reset the branch

--- amazon-related-crawler/amazon/amazon/spiders/books.py
+++ amazon-related-crawler/amazon/amazon/spiders/books.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import time
+from collections import deque
 
 
 class BooksSpider(scrapy.Spider):
     name = 'books'
     allowed_domains = ['amazon.com']
     start_urls = ['https://www.amazon.com/gp/product/1787300080/']
-    
+
     def __init__(self):
-        global l 
-        global i
-        l = []
-        i = 0
-    
-    def parse(self, response):
-        global l
-        global i
+        global relation_list 
+        global relation_list_length
+        global queue
+        queue = deque()
+        relation_list = []
+        relation_list_length = 0
+
+    def parse(page,response):
+        global relation_list 
+        global relation_list_length
+        global queue
         booktitle = response.xpath('//*[@id="productTitle"]/text()').extract_first().strip()
-        #print(booktitle)
+        print(booktitle)
 
         regex = re.compile('(Paperback|Hardcover)')
         formats = response.xpath('//div[@id="tmmSwatches"]').extract_first()
@@ -30,15 +35,17 @@ class BooksSpider(scrapy.Spider):
 
         links = response.css('li.a-carousel-card > div:first-child > a:first-child::attr(href)').extract()
         names = response.css('li.a-carousel-card > div:first-child > a:first-child > div:nth-child(2)::text').extract()
-        for number in range(len(names)):
-            names[number] = names[number].strip()
-        #print(links)
-        #print(names)
+
+        for i in range(len(names)):
+            names[i] = names[i].strip()
+
         nameCount = 0
         for link in links:
-            next_page = response.urljoin(link)
-            l.append((booktitle, names[nameCount]))
-            print(l[i])
-            i += 1
+            queue.append(response.urljoin(link))
+            relation_list.append((booktitle, names[nameCount]))
+            print(relation_list[relation_list_length])
+            relation_list_length += 1
             nameCount += 1
-            yield scrapy.Request(next_page, callback=self.parse)
+
+        while (len(queue) != 0):
+            yield scrapy.Request(queue.popleft(), callback=page.parse)
