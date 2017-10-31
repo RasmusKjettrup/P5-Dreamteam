17cd839e2bf82a2cebb5434fe2d541e5fa48dc88
Unknown <tksa15@student.aau.dk>
2017-10-31 14:15:35 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ WarehouseAI/WarehouseAI/AStarAlgorithm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseAI
+{
+    public class AStarAlgorithm
+    {
+        private List<Node> openSet;
+        private HashSet<Node> closedSet;
+/*
+        // Cost from start to current.
+        private float Calculate_gCost(Node n)
+        {
+            // Get the G-Cost to the previous node
+            float gCostToCameFrom = n.CameFrom.gCost;
+
+            //Find the edge from the previous node to the current node
+            Edge EdgeToCameFrom = n.GetEdgeToNeighbour(n.CameFrom);
+
+            //Add the weight of the new edge to the g-Cost of the previous node
+            return EdgeToCameFrom.weight + gCostToCameFrom;
+        }
+        */
+        // Estimated cost from current to goal node.
+        private float Calculate_hCost(Node n)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExploreNode(Node n)
+        {
+            //n.gCost = Calculate_gCost(n);
+            n.hCost = Calculate_hCost(n);
+        }
+
+        private Node GetNextNodeToInvestigate()
+        {
+            //find node with the lowest f-cost, else, search by h-cost
+            float lastFCost = 1000000, lastHCost = 1000000;
+            Node lastNode = null;
+            bool NodeFound = false;
+            foreach (Node n in openSet)
+            {
+                if (n.fCost < lastFCost)
+                {
+                    lastFCost = n.fCost;
+                    lastHCost = n.hCost;
+                    lastNode = n;
+                    NodeFound = true;
+                }
+                else if (n.fCost== lastFCost && lastHCost < n.hCost)
+                {
+                    lastFCost = n.fCost;
+                    lastHCost = n.hCost;
+                    lastNode = n;
+                    NodeFound = true;
+                }
+            }
+            if (!NodeFound)
+            {
+                throw new Exception("GetNextNodeToInvestigate: Did not find any node");
+            }
+            return lastNode;
+        }
+         
+        public List<Node> FindPath(Node startingNode, Node endingNode)
+        {
+            //Initialize the start node.
+            startingNode.gCost = 0;
+            startingNode.hCost = Calculate_hCost(startingNode);
+
+            Node current;
+            float temp_gCost;
+            openSet = new List<Node>();
+            closedSet = new HashSet<Node>();
+
+            openSet.Add(startingNode);
+
+            while (openSet.Count > 0)
+            {
+                //Find a node to investigate within the openSet
+                current = GetNextNodeToInvestigate();
+
+                if (current == endingNode)
+                    return BackTrackPath(endingNode);
+
+                //  Remove the currently investigated node from the openSet, 
+                //  and add it to the closedSet
+                openSet.Remove(current);
+                closedSet.Add(current);
+
+                foreach (Node neighbour in current.Neighbours)
+                {
+                    if (closedSet.Contains(neighbour)) continue;
+
+                    if (!openSet.Contains(neighbour)) openSet.Add(neighbour);
+
+                    temp_gCost = current.gCost + current.GetEdgeToNeighbour(neighbour).weight;
+                    if (temp_gCost >= neighbour.gCost) continue;
+
+                    neighbour.CameFrom = current;
+                    neighbour.gCost = temp_gCost;
+                    neighbour.hCost = Calculate_hCost(neighbour);
+                }
+            }
+
+            return null;
+        }
+
+        private List<Node> BackTrackPath(Node endNode)
+        {
+            Node current;
+            List<Node> path = new List<Node>();
+            current = endNode;
+            path.Add(current); 
+            while ((current = current.CameFrom) != null)
+            {
+                path.Add(current);
+            }
+            path.Reverse();
+            return path;
+        }
+
+        
+    }
+}
--- WarehouseAI/WarehouseAI/Node.cs
+++ WarehouseAI/WarehouseAI/Node.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace WarehouseAI
 {
@@ -7,11 +8,17 @@ namespace WarehouseAI
     {
         public float weight;
         public Node to;
+        public Node from;
     }
 
     public class Node
     {
+        #region AStar_Fields
         private Edge[] edges;
+        private float g_cost;
+        private float h_cost;
+        private Node cameFrom;
+        #endregion AStar_Fields
 
         public Node[] Neighbours
         {
@@ -22,6 +29,51 @@ namespace WarehouseAI
         {
             get { return edges; }
             set { edges = value; }
+        }
+
+        #region AStar_Props
+        public float fCost
+        {
+            get { return g_cost + h_cost; }
+        }
+        public float gCost
+        {
+            get { return g_cost; }
+            set { g_cost = value; }
+        }
+        public float hCost
+        {
+            get { return h_cost; }
+            set { h_cost = value; }
+        }
+        public Node CameFrom
+        {
+            get { return cameFrom;  }
+            set { cameFrom = value;  }
+        }
+        #endregion AStar_Props
+
+        public Edge GetEdgeToNeighbour(Node neighbour)
+        {
+            if (Neighbours.Contains<Node>(neighbour) != true)
+            {
+                throw new UnfittingNodeException("GetEdgeToNeighbour: The input node is not recognized as a neighbour");
+            }
+            foreach (Edge e in Edges)
+            {
+                if (e.to == neighbour || e.from == neighbour)
+                {
+                    return e;
+                }
+            }
+            throw new UnfittingNodeException("GetEdgeToNeighbour: The input node is recognized as a neighbour, but not found as part of any edge in Node.Edges");
+        }
+    }
+
+    public class UnfittingNodeException: Exception
+    {
+        public UnfittingNodeException(string message) : base(message)
+        {
         }
     }
 }
