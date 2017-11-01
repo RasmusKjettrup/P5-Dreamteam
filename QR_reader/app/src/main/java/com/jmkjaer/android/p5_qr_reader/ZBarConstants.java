fe3084446069bb351c5a7ccfc83f8ad3fd6bc798
jmkjaer <jmkjaer@hotmail.com>
2017-11-01 14:59:40 +0100

    Need to fix some unhandled exceptions

---
+++ QR_reader/app/src/main/java/com/jmkjaer/android/p5_qr_reader/ZBarConstants.java
@@ -0,0 +1,11 @@
+package com.jmkjaer.android.p5_qr_reader;
+
+/**
+ * TODO: Add a class header comment!
+ */
+public interface ZBarConstants {
+    public static final String SCAN_MODES = "SCAN_MODES";
+    public static final String SCAN_RESULT = "SCAN_RESULT";
+    public static final String SCAN_RESULT_TYPE = "SCAN_RESULT_TYPE";
+    public static final String ERROR_INFO = "ERROR_INFO";
+}
