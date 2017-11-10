ba7a28e0bce59a0b8cfd2154e8b6f625a8ec7ed7
jmkjaer <jmkjaer@hotmail.com>
2017-11-10 15:05:53 +0100

    Can now get title from ISBN barcode

--- QR_reader/app/src/main/java/p5/dreamteam/qr_reader/MainActivity.java
+++ QR_reader/app/src/main/java/p5/dreamteam/qr_reader/MainActivity.java
@@ -8,36 +8,37 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import p5.dreamteam.qr_reader.events.InputEventListener;
-import p5.dreamteam.qr_reader.events.DisconnectEventListener;
 
 import net.sourceforge.zbar.Symbol;
 
-public class MainActivity extends AppCompatActivity implements InputEventListener, DisconnectEventListener{
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity/* implements InputEventListener, DisconnectEventListener*/ {
     private static final int ZBAR_SCANNER_REQUEST = 0;
     private static final int ZBAR_QR_SCANNER_REQUEST = 1;
     private TextView _txtView;
-    private ConnectionHandler _connectionHandler;
+    private final static String TAG = "MainActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         _txtView = findViewById(R.id.txt_message);
-        _connectionHandler = new ConnectionHandler(this, this);
-        _connectionHandler.clientSetup();
+//        _connectionHandler = new ConnectionHandler(this, this);
+//        _connectionHandler.clientSetup();
     }
 
-    public void disconnectEventInvoke(){
-        // Event occurs whenever the client is disconnected from the server
-    }
-
-    public void inputEventInvoked(String string){
-        // Event occurs whenever the client recieves input from the server
-    }
+//    public void disconnectEventInvoke(){
+//        // Event occurs whenever the client is disconnected from the server
+//    }
+//
+//    public void inputEventInvoked(String string){
+//        // Event occurs whenever the client recieves input from the server
+//    }
 
     public void launchScanner(View v) {
         if (isCameraAvailable()) {
@@ -78,12 +79,23 @@ public class MainActivity extends AppCompatActivity implements InputEventListene
             case ZBAR_SCANNER_REQUEST:
             case ZBAR_QR_SCANNER_REQUEST:
                 if (resultCode == RESULT_OK) {
-                    _txtView.setText(data.getStringExtra(ZBarConstants.SCAN_RESULT));
-                } else if(resultCode == RESULT_CANCELED && data != null) {
+                    String result = null;
+                    GoogleAPIRequest request = new GoogleAPIRequest();
+                    try {
+                        result = request.execute(data.getStringExtra(ZBarConstants.SCAN_RESULT)).get();
+                    } catch (InterruptedException e) {
+                        Log.e(TAG, "Connection to server was interrupted.");
+                    } catch (ExecutionException e) {
+                        Log.e(TAG, "Could not execute due to connection interrupt.");
+                    }
+                    _txtView.setText(result);
+                } else if (resultCode == RESULT_CANCELED && data != null) {
                     String error = data.getStringExtra(ZBarConstants.ERROR_INFO);
-                    if(!TextUtils.isEmpty(error)) {
+                    if (!TextUtils.isEmpty(error)) {
                         Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(this, data.getStringExtra(ZBarConstants.ERROR_INFO), Toast.LENGTH_LONG).show();
                 }
                 break;
         }
