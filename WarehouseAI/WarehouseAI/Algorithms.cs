49b4e9c5a70abfead11cf06184466f330df21bde
Jeppe Lindberg <jepp3441@gmail.com>
2017-11-16 15:16:36 +0100

    Comments for the weight algorithm

--- WarehouseAI/WarehouseAI/Algorithms.cs
+++ WarehouseAI/WarehouseAI/Algorithms.cs
@@ -132,41 +132,69 @@ namespace WarehouseAI
                 cache = _cache;
             }
 
+            //frontiers describe the paths currently getting examined.
             List<Frontier> frontiers = new List<Frontier>();
-            frontiers.Add(new Frontier(new[] { graph[0] }, itemSet, 0));
+            //graph[0] is the dropoff point in the graph.
             Node dropoff = graph[0];
+            //Initially, the first fronter is selected, from the input graph. graph[0] is the dropoff point.
+            //The "books" field in Frontiers are the books that still need to be collected on the trip.
+            frontiers.Add(new Frontier(new[] { dropoff }, itemSet, 0));
 
+            //While the cache element that we are interested in are marked for updating, keep exploring frontiers.
             while (cache[itemSet].Marked)
             {
+                //The resultingfrontier is the result of this specific iteration of the while loop.
                 Frontier resultingFrontier = new Frontier(null, null, int.MaxValue);
+                //Loop through each frontier in frontiers
                 foreach (Frontier frontier in frontiers)
                 {
+                    //Find the last node of the route of the froontier currently getting examined.
                     Node lastNode = frontier.route.Last();
+
+                    //Foreach node neighbour of the last node, that is a shelf, and contains at least 
+                    //one book that we are interested in...
                     foreach (Shelf neighbour in lastNode.Neighbours.Where(n => n is Shelf).Cast<Shelf>()
                         .Where(s => s.Contains(frontier.books)))
                     {
+                        //Find the distance between the neighbour and the last node.
                         float distance = Distance(distanceMap, lastNode, neighbour);
+                        //If the route that the current frontiers route + the neighbour has not been explored already,
+                        //and it is more efficient than the current resultingfrontier...
                         if (NotExplored(frontiers.ToArray(), frontier.route.Append(neighbour).ToArray())
                             && frontier.weight + distance < resultingFrontier.weight)
                         {
+                            //Set the resultingfrontier to be a new frontier created form the current frontier.
+                            //The route is the old route with the neighbour appended, the array of books are the
+                            //old array of books with the neighbour's books subtracted, and the weight
+                            //is the old weight plus the distance.
                             resultingFrontier = new Frontier(frontier.route.Append(neighbour).ToArray(),
                                 frontier.books.Where(i => !neighbour.Contains(i)).ToArray(),
                                 frontier.weight + distance);
                         }
                     }
+
                     CacheElement c;
+                    //If the cache contains the current set of collected books, and is marked, the weight in the cache
+                    //need to be updated.
                     if (cache.TryGet(itemSet.Except(frontier.books).ToArray(), out c) && c.Marked)
                     {
+                        //The distance between the last node and the dropoff node is found...
                         float distance = Distance(distanceMap, lastNode, dropoff);
+                        //And used to evaluate the weight of the resultingfrontier
                         if (frontier.weight + distance <= resultingFrontier.weight)
                         {
+                            //If it is the most efficient, create a new frontier that routes through each book,
+                            //and moves back to the dropoff point.
                             resultingFrontier = new Frontier(frontier.route.Append(dropoff).ToArray(),
                                 frontier.books,
                                 frontier.weight + distance);
                         }
                     }
                 }
+                //Add the current resultingfrontier to the set of frontiers, to evaluate in the next iteration.
                 frontiers.Add(resultingFrontier);
+                //If the last node in the resultingfrontier is the dropoff point then we know it is a full cycle and
+                //should update the element in the cache that represents the set of items.
                 if (resultingFrontier.route.Last() == dropoff)
                 {
                     CacheElement c = cache[itemSet.Except(resultingFrontier.books).ToArray()];
@@ -176,6 +204,7 @@ namespace WarehouseAI
                 }
             }
 
+            //Whenever the cache element for the requested itemset is updated, the weight of the cache element is returned.
             return cache[itemSet].Weight;
         }
 
