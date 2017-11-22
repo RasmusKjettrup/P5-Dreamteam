6f16d7ec2a76af544b3126c9c31147e18075fd69
Fynfner <kennetnlarsen@gmail.com>
2017-11-22 14:07:47 +0100

    Unit testing: Minor naming fix

---
+++ WarehouseAI/WarehouseAITest/ImportanceAlgorithmTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WarehouseAI;
+using WarehouseAI.Representation;
+
+namespace WarehouseAITest
+{
+    [TestFixture]
+    class ImportanceAlgorithmTests
+    {
+        public static List<Item> GenerateTestData(int numberOfItems, int numberOfRelations)
+        {
+            List<Item> items = new List<Item>();
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                items.Add(new Item(i, "item" + i.ToString()));
+            }
+            for (int i = 0; i < numberOfRelations; i++)
+            {
+                items[i % numberOfItems].AddOutgoingRelation(items.Find(item => item.Id == (i + 1) % numberOfItems));
+            }
+            return items;
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(1, 0)]
+        [TestCase(1, 1)]
+        [TestCase(5, 0)]
+        [TestCase(5, 1)]
+        [TestCase(5, 5)]
+        [TestCase(5, 10)]
+        [TestCase(5, 50)]
+        [TestCase(5, 100)]
+        [TestCase(10, 0)]
+        [TestCase(10, 1)]
+        [TestCase(10, 5)]
+        [TestCase(10, 10)]
+        [TestCase(10, 50)]
+        [TestCase(10, 100)]
+        [TestCase(50, 0)]
+        [TestCase(50, 1)]
+        [TestCase(50, 5)]
+        [TestCase(50, 10)]
+        [TestCase(50, 50)]
+        [TestCase(50, 100)]
+        [TestCase(100, 0)]
+        [TestCase(100, 1)]
+        [TestCase(100, 5)]
+        [TestCase(100, 10)]
+        [TestCase(100, 50)]
+        [TestCase(100, 100)]
+        public void ImportanceAlgorithmTest(int numberOfItems, int numberOfRelations)
+        {
+            // Arrange 
+            List<Item> items = GenerateTestData(numberOfItems, numberOfRelations);
+
+            // Act
+            float expected;
+            try
+            {
+                expected = Algorithms.Importance(items.ToArray());
+            }
+            catch
+            {
+                expected = 0;
+            }
+            // Assert
+            if (numberOfItems == 0)
+            {
+                Assert.Throws<ArgumentException>(() => Algorithms.Importance(items.ToArray()));
+            }
+            else
+            {
+                float actual = (float)numberOfRelations / numberOfItems;
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
