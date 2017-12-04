ebfe2e38eb82dcd9ff05a7ab3ab4732a2060cd8f
jmkjaer <jmkjaer@hotmail.com>
2017-12-04 13:21:28 +0100

    Code cleanup

--- WarehouseAI/WarehouseAI/Program.cs
+++ WarehouseAI/WarehouseAI/Program.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Sockets;
-using System.Runtime.Remoting.Channels;
-using System.Text;
-using System.Threading.Tasks;
-using WarehouseAI.Representation;
+﻿using WarehouseAI.Representation;
 using WarehouseAI.UI;
 
 namespace WarehouseAI
 {
     public static class Program
     {
-        private const string CommandList = "\\help";
-        private static readonly Dictionary<string, Delegate> Commands = new Dictionary<string, Delegate>();
-
         private static void Main(string[] args)
         {
             IController consoleController = new ConsoleController();
@@ -32,21 +20,5 @@ namespace WarehouseAI
             consoleController.Start(args);
             
         }
-       
-//        /// <summary>
-//        /// Prints the list of clients
-//        /// </summary>
-//        private static void ListAllClients()
-//        {
-//            if (_server.ClientSockets.Count < 1)
-//            {
-//                Console.WriteLine("There is currently no clients connected to the server");
-//                return;
-//            }
-//            foreach (Socket serverClientSocket in _server.ClientSockets)
-//            {
-//                Console.WriteLine(serverClientSocket.RemoteEndPoint.ToString());
-//            }
-//        }
     }
 }
